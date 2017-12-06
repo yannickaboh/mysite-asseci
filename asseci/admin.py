85de5aae431c46578325fbd6846cd2c85463011d
DR YANN <DR YANN>
2017-12-06 11:09:08 +0100

    Modification des templates annonce et evenement detail, site asseci

--- asseci/admin.py
+++ asseci/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from . import models
+from asseci import models
+from django.conf import settings
 
 # Register your models here.
+
+
+admin.site.register(models.Evenement, )
+admin.site.register(models.Annonce, )
+admin.site.register(models.ThemeForum, )
+admin.site.register(models.SujetForum, )
+admin.site.register(models.Commentaires, )
+
+
+admin.site.register(models.Board, )
+admin.site.register(models.Topic, )
